e12ae2019f6650eb7aad4ffb625b50e44a4041fc
Simon Luginbuehl <simon.luginbuehl@ict-campus.net>
2024-10-07 16:02:37 +0200

    Starded Coding the guess mecanism and added Stard Graphics as well as the Word list

--- Main.py
+++ Main.py
@@ -1,3 +1,60 @@
 import hang
 import random
+def startsceen():
+    print("|---Welcome to Python Hangman---|")
+    print("| #######   #     #             |")
+    print("|    #      ##   ##             |")
+    print("|    #      # # # #             |")
+    print("|    #      #  #  #             |")
+    print("|                               |")
+    print("|                               |")
+    print("|                               |")
+    print("|                               |")
+    print("|                               |")
+    print("|                               |")
+    print("|-------------------------------|")
 
+Wörterliste=[
+"Algorithmus",
+"API",
+"Cloud",
+"Datenbank",
+"Debugging",
+"Frontend",
+"Backend",
+"Netzwerk",
+"Server",
+"Virtualisierung",
+"Container",
+"DevOps",
+"Verschlüsselung",
+"Skripting",
+"Middleware",
+"Architektur",
+"Benutzeroberfläche",
+"Framework",
+"Microservices",
+"Repository"]
+
+gamestart= 0
+def guess(Wörterliste):
+    if gamestart == 0:
+        rnum = random.randint(0,19)
+        word = Wörterliste.pop(rnum)
+        listedWord = list(word)
+    else:
+        letter = str(input("Gib bitte den Buchstaben den du raten möchtest ein:"))
+        
+        lettertoshowfound= listedWord.pop(posinlist)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+startsceen()
+guess(Wörterliste)
