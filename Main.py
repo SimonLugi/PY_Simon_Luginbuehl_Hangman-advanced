4668ddbf0beaffb9f531e54f197de0aa5b582392
Simon Luginbuehl <simon.luginbuehl@ict-campus.net>
2024-10-07 17:16:34 +0200

    Updated the main mechanism

--- Main.py
+++ Main.py
@@ -34,22 +34,31 @@ Wörterliste=[
 
 lives= 10 
 gamestart= 0
+shtext=[]
 def guess(Wörterliste,gamestart):
     if gamestart == 0:
         rnum = random.randint(0,19)
         word = Wörterliste.pop(rnum)
         listedWord = list(word)
         gamestart=1
+    gamestart=1
     letter = str(input("Gib bitte den Buchstaben den du raten möchtest ein:"))
-    for i in listedWord:
-        if(i.find(letter) == 0 or i.find(letter.lower()) == 0):
-            posinlist=listedWord.index(letter)
-            lettertoshowfound= listedWord.pop(posinlist)
-            print(lettertoshowfound)
+    if letter in listedWord or letter.lower() in listedWord:
+        tfmi(letter,listedWord)
+    else:
+        print(f"Letter '{letter}' not found in the word.")
+        damage(lives)  
+
+def tfmi(letter,listedWord,shtext):
+    for i in range(0,len(listedWord)):
+        if letter in listedWord or letter.lower() in listedWord:
+            posinlist = listedWord.index(letter)
+            lettertoshowfound = listedWord.pop(posinlist)  
+            shtext = shtext.insert(posinlist,lettertoshowfound)  
         else:
-            damage()
-            
-def damage():            
+            guess(Wörterliste,gamestart)        
+        
+def damage(lives):            
     print("Leider ist der Buchtabe nicht in dem Gesuchten wort")
     lives = lives - 1
     guess(Wörterliste,gamestart)
