d8fafab098298e6435f0b1f7882e6448b4448c17
Simon Luginbuehl <simon.luginbuehl@ict-campus.net>
2024-10-08 08:35:34 +0200

    changed main part with word generation

--- Main.py
+++ Main.py
@@ -23,48 +23,66 @@ Wörterliste=[
 "Microservices",
 "Repository"]
 
-lives= 0 
-gamestart= 0
+lives= 6 
+gamestart=0
+gpUpdate= 0
 shtext=[]
-def guess(Wörterliste,gamestart):
-    if gamestart == 0:
-        rnum = random.randint(0,19)
-        word = Wörterliste.pop(rnum)
-        listedWord = list(word)
-        gamestart=1
+def generateWord(Wörterliste):
+    rnum = random.randint(0,19)
+    word = Wörterliste.pop(rnum)
+    listedWord = list(word)
+    print(listedWord)
     gamestart=1
+    print(gamestart)
+def guess(listedWord):
     letter = str(input("Gib bitte den Buchstaben den du raten möchtest ein:"))
     if letter in listedWord or letter.lower() in listedWord:
-        tfmi(letter,listedWord)
+        print(listedWord)
+        tfmi(shtext,letter,listedWord)
     else:
-        damage(lives)  
+        print(listedWord)
+        damage(lives,gpUpdate)
+          
 def tfmi(letter,listedWord,shtext):
     for i in range(0,len(listedWord)):
         if letter in listedWord or letter.lower() in listedWord:
             posinlist = listedWord.index(letter)
             lettertoshowfound = listedWord.pop(posinlist)  
-            shtext = shtext.insert(posinlist,lettertoshowfound)  
+            shtext[posinlist]= lettertoshowfound  
         else:
-            guess(Wörterliste,gamestart)             
-def damage(lives):            
-    print("Leider ist der Buchtabe nicht in dem Gesuchten wort")
-    lives = lives + 1
-    guess(Wörterliste,gamestart)
+            guess(listedWord)             
+def damage(lives,gpUpdate,listedWord):
+    if lives == 0:
+        death()
+    else:           
+        print("Leider ist der Buchtabe nicht in dem Gesuchten wort")
+        lives = lives - 1
+        gpUpdate = gpUpdate + 1
+        guess(listedWord) #Applying damage and send update to graphics
+        graphics
 def death():
     print(hang.death)
-   
+    print("You Died")
+def graphics(gpUpdate):
+    print(hang.man[gpUpdate])
+    
+    
+    
+def main():
+    hang.startsceen()
+    graphics(gpUpdate)
+    generateWord(Wörterliste)
+    guess(Wörterliste,gamestart)    
     
 
 
 
     
     
+main()
     
     
     
     
     
     
-    
-hang.startsceen()
-guess(Wörterliste,gamestart)
