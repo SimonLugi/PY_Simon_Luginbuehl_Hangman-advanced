6fefb686f91ab64a9cdfb5d3efd1ba3a7039efca
Simon Luginbuehl <simon.luginbuehl@ict-campus.net>
2024-10-09 17:03:40 +0200

    update save

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import hang
 import random
 
-lives= 6 
+lives=int(6)
 gamestart=0
 gpUpdate= 0
 shtext=[]
@@ -28,35 +28,32 @@ Wörterliste=[
 "Microservices",
 "Repository"]
 
-
-def generateWord(Wörterliste):
+def generateWord(Wörterliste: list):
     rnum = random.randint(0,19)
-    word = Wörterliste.pop(rnum)
+    word: str = Wörterliste.pop(rnum)
     word = word.upper()
     listedWord = list(word)
     print(listedWord)
-    gamestart=1
-    print(gamestart)
-    return(listedWord,word)
+    return(listedWord, word)
 
 def showgen(word):
     for i in range(0,len(list(word))):
         shtext.append("_")
         
-def guess(listedWord,word):
+def guess(listedWord, word):
     graphics(gpUpdate,shtext)
-    letter = str(input("Gib bitte den Buchstaben den du raten möchtest ein:"))
+    letter = input("Gib bitte den Buchstaben den du raten möchtest ein:")
     letter = letter.upper()
     if letter in listedWord:
         tfmi(letter=letter,listedWord=listedWord,word=word,shtext=shtext)
     elif listedWord ==  []:
         win()
     elif letter not in listedWord:
-        damage(lives,gpUpdate,listedWord,word)
+        damage(listedWord,word,lives,gpUpdate)
     #elif letter in shtext :
         
 def tfmi(letter,listedWord,word,shtext):
-    for i in range(0,len(list(word))):
+    for _ in range(0,len(list(word))):
         if letter in listedWord:
             posinlist = listedWord.index(letter)
             lettertoshowfound = listedWord.pop(posinlist)
@@ -66,28 +63,29 @@ def tfmi(letter,listedWord,word,shtext):
         else:
             guess(listedWord=listedWord,word=word)
             
-def damage(listedWord,word,lives=lives,gpUpdate=gpUpdate):
+def damage(listedWord, word, lives, gpUpdate):
     if lives == 0:
         death()
     else:           
         print("Leider ist der Buchtabe nicht in dem Gesuchten wort! 🗡- 🗡- AUA")
         lives = lives - 1
-        print("Du hast noch ",lives," leben")
         gpUpdate = gpUpdate + 1
-        guess(listedWord,word) #Applying damage and send update to graphics
-        return(gpUpdate,lives)
+        print("Du hast noch ",lives," leben")
+        guess(listedWord,word)
+    return(gpUpdate,lives)
+        
         
 def death():
     print(hang.death)
     print("You Died")
     
-
-def graphics(gpUpdate=gpUpdate,shtext=shtext):
+def graphics(gpUpdate, shtext):
     print(hang.man[gpUpdate])
     print(shtext)
 
 def win():
     print(chr(27) + "[2J")
+    print("you have won")
     
 def main():
     hang.startsceen()
@@ -95,4 +93,5 @@ def main():
     showgen(word)
     guess(listedWord, word)
     
-main()
+if __name__ == "__main__":
+    main()
